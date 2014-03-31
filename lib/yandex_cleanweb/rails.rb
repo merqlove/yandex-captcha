2087da06110c81b23bc757d2120b650cb8b9e9c0
Alexander Merkulov <sasha@merqlove.ru>
2014-04-01 01:00:22 +0400

    Fix

--- lib/yandex_cleanweb/rails.rb
+++ lib/yandex_cleanweb/rails.rb
@@ -1,4 +1,6 @@
+require 'rails/all'
 require "yandex_cleanweb"
+require 'yandex_cleanweb/engine'
 
 ActionView::Base.send(:include, YandexCleanweb::ClientHelper)
 ActionController::Base.send(:include, YandexCleanweb::Verify)
