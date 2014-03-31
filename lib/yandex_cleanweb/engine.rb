137eaffd9da71612bb8e15958d562ca0736acde9
Alexander Merkulov <sasha@merqlove.ru>
2014-04-01 00:44:34 +0400

    Fix

--- lib/yandex_cleanweb/engine.rb
+++ lib/yandex_cleanweb/engine.rb
@@ -2,8 +2,16 @@ require "yandex_cleanweb"
 module YandexCleanweb
 
   class Engine < Rails::Engine
+    isolate_namespace YandexCleanweb
+    engine_name 'yandex_cleanweb'
 
-    initialize "setup config" do
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    config.after_initialize do
+      Rails.application.routes_reloader.reload!
+    end
+
+    initializer "setup config" do
       begin
         ActionView::Base.send(:include, ::YandexCleanweb::ClientHelper)
         ActionController::Base.send(:include, ::YandexCleanweb::Verify)
