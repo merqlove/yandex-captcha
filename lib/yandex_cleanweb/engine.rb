d2852f4e37b507ed5350d140d9b962a0d81fe3c0
Alexander Merkulov <sasha@merqlove.ru>
2014-04-01 18:09:02 +0400

    Captcha assets precompile fix

--- lib/yandex_cleanweb/engine.rb
+++ lib/yandex_cleanweb/engine.rb
@@ -4,16 +4,19 @@ module YandexCleanweb
   class Engine < Rails::Engine
     isolate_namespace YandexCleanweb
     engine_name 'yandex_cleanweb'
+    config.mount_at = '/'
+
+    if Rails.version >= '3.1'
+      initializer :assets do |config|
+        Rails.application.config.assets.precompile += %w( yandex_cleanweb/captcha.js )
+      end
+    end
 
     initializer "setup config" do
       begin
         ActionView::Base.send(:include, ::YandexCleanweb::ClientHelper)
         ActionController::Base.send(:include, ::YandexCleanweb::Verify)
       end
-    end
-
-    initializer "yandex_cleanweb.assets.precompile" do |app|
-      app.config.assets.precompile += %w(captcha.js)
     end
 
   end
--- lib/yandex_cleanweb/version.rb
+++ lib/yandex_cleanweb/version.rb
@@ -1,3 +1,3 @@
 module YandexCleanweb
-  VERSION = "0.3.0b3"
+  VERSION = "0.3.0b4"
 end
