6c2769893db316f3338dfe8ce599e2ba36fa123b
Alexander Merkulov <sasha@merqlove.ru>
2014-04-01 11:57:44 +0400

    Respond with captcha

--- app/controllers/yandex_cleanweb/captcha_controller.rb
+++ app/controllers/yandex_cleanweb/captcha_controller.rb
@@ -1,9 +1,8 @@
 module YandexCleanweb
   class CaptchaController < YandexCleanweb::ApplicationController
+    respond_to :json
     def show
-      respond_to do |format|
-        format.json { render :json => YandexCleanweb::Verify.get_captcha }
-      end
+      respond_with(YandexCleanweb::Verify.get_captcha)
     end
   end
 end
