01d705b6cd696f2affe48f3cd4b17f4d9e34f536
Alexander Merkulov <sasha@merqlove.ru>
2014-05-25 04:16:46 +0400

    Cleanup

--- lib/yandex_cleanweb/helpers/sinatra.rb
+++ lib/yandex_cleanweb/helpers/sinatra.rb
@@ -1,16 +1,15 @@
 module YandexCleanweb
   module Helpers
     module Sinatra
-    # Your public API can be specified in the +options+ hash or preferably
-    # using the Configuration.
-    def captcha_tags(options = {})
-      if options[:ajax]
-        render :erb, settings.captcha_ajax_template.to_s.to_sym, {layout: false}
-      else
-        captcha = YandexCleanweb::Verify.get_captcha
-        render :erb, settings.captcha_template.to_s.to_sym, {layout: false}, { captcha: captcha, noscript: options[:noscript] } if captcha
-      end
-    end # captcha_tags
+
+      def captcha_tags(options = {})
+        if options[:ajax]
+          render :erb, settings.captcha_ajax_template.to_s.to_sym, {layout: false}
+        else
+          captcha = YandexCleanweb::Verify.get_captcha
+          render :erb, settings.captcha_template.to_s.to_sym, {layout: false}, { captcha: captcha, noscript: options[:noscript] } if captcha
+        end
+      end # captcha_tags
 
     end
   end
