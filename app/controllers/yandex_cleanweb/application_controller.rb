08b0b1cdc30846b5c0255ea753941d1abe6d41c5
Alexander Merkulov <sasha@merqlove.ru>
2014-05-25 14:51:41 +0400

    Test before filters disable

--- app/controllers/yandex_cleanweb/application_controller.rb
+++ app/controllers/yandex_cleanweb/application_controller.rb
@@ -1,6 +1,7 @@
 module YandexCleanweb
   class ApplicationController < ::ApplicationController
     layout false
-    skip_before_filter :change_country, :set_random_amount_of_users
+    before_filter :setup
+    _process_action_callbacks.map(&:filter)
   end
 end
