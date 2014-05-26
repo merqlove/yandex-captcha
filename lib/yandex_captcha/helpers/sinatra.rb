6b3be7eb63830b4fe8bcc6447afe05407a9a189e
Alexander Merkulov <sasha@merqlove.ru>
2014-05-26 16:42:33 +0400

    Used Sinatra current_engine instead :erb hardcode.

--- lib/yandex_captcha/helpers/sinatra.rb
+++ lib/yandex_captcha/helpers/sinatra.rb
@@ -3,11 +3,12 @@ module YandexCaptcha
     module Sinatra
 
       def captcha_tags(options = {})
+        template = settings.captcha_ajax_template.to_s.to_sym
         if options[:ajax]
-          render :erb, settings.captcha_ajax_template.to_s.to_sym, {layout: false}
+          render(current_engine, template, {layout: false}).to_s.html_safe
         else
           captcha = YandexCaptcha::Verify.get_captcha
-          render :erb, settings.captcha_template.to_s.to_sym, {layout: false}, { captcha: captcha, noscript: options[:noscript] } if captcha
+          render(current_engine, template, {layout: false}, { captcha: captcha, noscript: options[:noscript] }).to_s.html_safe if captcha
         end
       end
 
