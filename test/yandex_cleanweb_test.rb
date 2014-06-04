e7dfb62aba699722aa91193b381000aea1f33b16
Alexander Merkulov <sasha@merqlove.ru>
2014-06-04 18:15:22 +0400

    Fixed tests

--- test/yandex_cleanweb_test.rb
+++ test/yandex_cleanweb_test.rb
@@ -5,13 +5,15 @@ describe YandexCaptcha do
 
   context "without api key" do
     before do
-      YandexCaptcha.api_key = nil
+      YandexCaptcha.configure do |config|
+        config.api_key  = nil
+      end
     end
 
     describe "#spam?" do
       it "raise an error" do
         -> {
-          YandexCaptcha.spam?("anything")
+          YandexCaptcha::Verify.spam?("anything")
         }.must_raise YandexCaptcha::NoApiKeyException
       end
     end
@@ -19,7 +21,7 @@ describe YandexCaptcha do
     describe "#get_captcha" do
       it "raise an error" do
         -> {
-          YandexCaptcha.get_captcha("anything")
+          YandexCaptcha::Verify.get_captcha("anything")
         }.must_raise YandexCaptcha::NoApiKeyException
       end
     end
@@ -27,7 +29,7 @@ describe YandexCaptcha do
     describe "#valid_captcha?" do
       it "raise an error" do
         -> {
-          YandexCaptcha.valid_captcha?("anything", "anything", 123)
+          YandexCaptcha::Verify.valid_captcha?("anything", "anything", 123)
         }.must_raise YandexCaptcha::NoApiKeyException
       end
     end
@@ -36,12 +38,14 @@ describe YandexCaptcha do
 
   context "with empty api key" do
     before do
-      YandexCaptcha.api_key = ""
+      YandexCaptcha.configure do |config|
+        config.api_key = ""
+      end
     end
 
     it "raise an error" do
       -> {
-        YandexCaptcha.spam?("anything")
+        YandexCaptcha::Verify.spam?("anything")
       }.must_raise YandexCaptcha::NoApiKeyException
     end
   end
@@ -49,25 +53,27 @@ describe YandexCaptcha do
   context "with api key" do
 
     before do
-      YandexCaptcha.api_key = "cw.1.1.20121227T080449Z.51de1ee126e5ced6.f4f417fb55727520d7e39b00cf5393d4b1ca5e78"
+      YandexCaptcha.configure do |config|
+        config.api_key = "cw.1.1.20121227T080449Z.51de1ee126e5ced6.f4f417fb55727520d7e39b00cf5393d4b1ca5e78"
+      end
     end
 
     describe "#spam?" do
 
       describe "simple check" do
         it "works" do
-          YandexCaptcha.spam?("фраза").must_equal false
-          YandexCaptcha.spam?("недорого увеличение пениса проститутки").must_equal false
+          YandexCaptcha::Verify.spam?("фраза").must_equal false
+          YandexCaptcha::Verify.spam?("недорого увеличение пениса проститутки").must_equal false
         end
       end
 
       describe "advanced mode" do
         it "works" do
-          YandexCaptcha.spam?(body_plain: "my text", ip: "80.80.40.3").must_equal false
+          YandexCaptcha::Verify.spam?(body_plain: "my text", ip: "80.80.40.3").must_equal false
         end
 
         it "with some html" do
-          result = YandexCaptcha.spam?(body_html: "some spam <a href='http://spam.com'>spam link</a>")
+          result = YandexCaptcha::Verify.spam?(body_html: "some spam <a href='http://spam.com'>spam link</a>")
 
           result[:id].wont_be_empty
           result[:links].must_be_empty
@@ -78,13 +84,13 @@ describe YandexCaptcha do
     describe "#get_captcha + #valid_captcha?" do
 
       it "works for not valid captchas" do
-        result = YandexCaptcha.spam?(body_html: "some spam <a href='http://spam.com'>spam link</a>")
-        captcha = YandexCaptcha.get_captcha(result[:id])
+        result = YandexCaptcha::Verify.spam?(body_html: "some spam <a href='http://spam.com'>spam link</a>")
+        captcha = YandexCaptcha::Verify.get_captcha(result[:id])
 
         captcha[:url].wont_be_empty
         captcha[:captcha].wont_be_empty
 
-        valid = YandexCaptcha.valid_captcha?(result[:id], captcha[:captcha], "1234")
+        valid = YandexCaptcha::Verify.valid_captcha?(result[:id], captcha[:captcha], "1234")
         valid.must_equal false
       end
     end
@@ -96,7 +102,7 @@ describe YandexCaptcha do
 
       it "check for spam" do
         FakeWeb.register_uri(:post, "http://cleanweb-api.yandex.ru/1.0/check-spam", body: "")
-        proc { YandexCaptcha.spam?(body_plain: "any text") }.must_raise(YandexCaptcha::BadResponseException)
+        proc { YandexCaptcha::Verify.spam?(body_plain: "any text") }.must_raise(YandexCaptcha::BadResponseException)
       end
     end
   end
