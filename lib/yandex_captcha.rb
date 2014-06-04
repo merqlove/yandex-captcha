6ce54f758a0465a1aed279d379a7ae2b4fe1c4c5
Alexander Merkulov <sasha@merqlove.ru>
2014-06-04 19:50:04 +0400

    Refactor main module. Version bump. 0.4.1

--- lib/yandex_captcha.rb
+++ lib/yandex_captcha.rb
@@ -4,16 +4,12 @@ require 'yandex_captcha/verify'
 module YandexCaptcha
   class NoApiKeyException < Exception; end
   class BadResponseException < Exception; end
+  class YandexCaptchaError < StandardError; end
 
   API_URL = 'http://cleanweb-api.yandex.ru/1.0'
   CAPTCHA_TYPE = 'std'
   HANDLE_TIMEOUTS_GRACEFULLY      = true
   SKIP_VERIFY_ENV = ['test', 'cucumber']
-
-  # Gives access to the current Configuration.
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
 
   # Allows easy setting of multiple configuration options. See Configuration
   # for all available options.
@@ -22,7 +18,11 @@ module YandexCaptcha
     yield(config)
   end
 
-  class YandexCleanwebError < StandardError
+  private
+
+  # Gives access to the current Configuration.
+  def self.configuration
+    @configuration ||= Configuration.new
   end
 end
 
